0c370dfcd183556fd384735e6e09f468ed69c19b
Andrew Harcourt <andrewh@uglybugger.org>
2017-05-05 20:53:03 +1000

    Added colours

--- LevelUp2017/Program.cs
+++ LevelUp2017/Program.cs
@@ -8,14 +8,18 @@ namespace LevelUp2017
         private static void Main(string[] args)
         {
             var myFriends = new List<string>();
-            for (var i = 0; i < 3; i++)
+            for (var i = 0; i < 4; i++)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"Round {i} of {3}");
                 Console.WriteLine("What is your name?");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 var personName = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"Hello, {personName}!");
                 Console.WriteLine($"Isn't it a lovely {(DateTime.Now.Hour < 12 ? "morning" : "afternoon")}?");
                 Console.WriteLine("Do you like cookies?");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 var likesCookies = bool.Parse(Console.ReadLine());
                 if (likesCookies)
                 {
@@ -24,6 +28,7 @@ namespace LevelUp2017
                 }
                 else
                 {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Get away from me, you sick person.");
                 }
                 Console.WriteLine();
